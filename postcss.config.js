8626e665162c351d81affb17e023915f866b84e0
Shota Matsuda <shota@takram.com>
2018-05-09 13:20:05 +0900

    removed map configuration

--- postcss.config.js
+++ postcss.config.js
@@ -2,11 +2,6 @@
 // Copyright (C) 2017-Present Shota Matsuda
 
 module.exports = {
-  map: {
-    inline: false,
-    annotation: true,
-    sourcesContent: true
-  },
   plugins: {
     autoprefixer: {}
   }
