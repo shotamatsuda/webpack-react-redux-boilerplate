c66003ea126a9e3c4694ef1366a9c3a082a88e98
Shota Matsuda <shota@takram.com>
2018-07-10 20:38:55 +0900

    use redux-action’s handleActions

--- src/js/reducer.js
+++ src/js/reducer.js
@@ -1,34 +1,40 @@
 // The MIT License
 // Copyright (C) 2017-Present Shota Matsuda
 
-import {} from './actions'
+import { handleActions } from 'redux-actions'
+
+import {
+  INCREMENT,
+  WILL_INCREMENT,
+  DID_INCREMENT
+} from './actions'
 
 const initialState = {
   syncCount: 0,
   asyncRequests: 0,
-  asyncCount: 0,
+  asyncCount: 0
 }
 
-export default function reducer (state = initialState, action) {
-  switch (action.type) {
-    case 'INCREMENT_SYNC':
-      return {
-        ...state,
-        syncCount: state.syncCount + 1,
-      }
-    case 'BEGIN_INCREMENT_ASYNC':
-      return {
-        ...state,
-        asyncRequests: state.asyncRequests + 1,
-      }
-    case 'END_INCREMENT_ASYNC':
-      return {
-        ...state,
-        asyncRequests: state.asyncRequests - 1,
-        asyncCount: state.asyncCount + 1,
-      }
-    default:
-      break
+export default handleActions({
+  [INCREMENT]: (state, action) => {
+    return {
+      ...state,
+      syncCount: state.syncCount + 1
+    }
+  },
+
+  [WILL_INCREMENT]: (state, action) => {
+    return {
+      ...state,
+      asyncRequests: state.asyncRequests + 1
+    }
+  },
+
+  [DID_INCREMENT]: (state, action) => {
+    return {
+      ...state,
+      asyncRequests: state.asyncRequests - 1,
+      asyncCount: state.asyncCount + 1
+    }
   }
-  return state
-}
+}, initialState)
